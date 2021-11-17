350ea15953f9d652abed00670beb6942f8b8b021
TACITVM <tacitvm@tuta.io>
2021-11-17 02:32:18 -0300

    v1.0

---
+++ realtime-changes.js
@@ -0,0 +1,41 @@
+const hover1 = document.getElementById("button1");
+const hover2 = document.getElementById("button2");
+const hover3 = document.getElementById("button3");
+const hover4 = document.getElementById("button4");
+const hover5 = document.getElementById("button5");
+const hover6 = document.getElementById("button6");
+let textContainer = document.getElementById("interchangableText");
+const defTextDisplayed = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sit amet vulputate nibh. Nam vel dui aliquam, sollicitudin neque eu, mollis lacus. Praesent egestas velit a justo mollis fringilla. Donec ullamcorper luctus urna, et efficitur tellus rutrum non. Ut ultrices varius ante, ut pellentesque erat tristique eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet placerat lacus.';
+
+
+const soporteTecnico = "Brindamos soporte tecnico tanto a empresas como a particulares, ... ";
+const seguridadVigilancia = "";
+const mantenimientoActualizacion = "Holaaaaaaaaaaaaaaaaaaaaaaaaaaa a a a a a";
+const backups = "wqeeeeeeeeeeeeeeeeee";
+const accesorios = "accesorios";
+const lastItem = "";
+
+
+function hoverText(currentBtn) {
+    let toEval = currentBtn.id;
+    switch (toEval) {
+        case 'button1':
+            textContainer.innerHTML = soporteTecnico; break;
+        case 'button2':
+           textContainer.innerHTML = seguridadVigilancia; break;
+        case 'button3':
+            textContainer.innerHTML = mantenimientoActualizacion; break;
+        case 'button4':
+            textContainer.innerHTML = backups; break;
+        case 'button5':
+            textContainer.innerHTML = accesorios; break;
+        case 'button6':
+            textContainer.innerHTML = lastItem; break;
+        default:
+            textContainer.innerHTML = defTextDisplayed; break;
+}
+}
+
+function defText() {
+    textContainer.innerHTML = defTextDisplayed;
+}
