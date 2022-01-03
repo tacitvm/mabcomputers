3055b2e8c7317e972c0b8e1a5155331062542468
TACITVM <tacitvm@tuta.io>
2022-01-03 09:36:41 -0300

    more mobile changes

--- realtime-changes.js
+++ realtime-changes.js
@@ -5,9 +5,6 @@ const click4 = document.getElementById("button4");
 const click5 = document.getElementById("button5");
 const click6 = document.getElementById("button6");
 let textContainer = document.getElementById("interchangableText");
-
-
-
 const soporteTecnico = "Nos encargamos de identificar el problema tanto de software como hardware para brindarte la mejor solución a tu alcance. Reparamos computadoras de escritorio, notebooks y netbooks. Sí tenés un problema de software con algún programa, incluso podemos solucionarlo remotamente. (aca va contacto mas rapido, sea telefono, whatsapp, etc) con un botón CTA";
 const seguridadVigilancia = "Trabajamos con equipos de las mejores marcas para darte lo último en tecnología y ofrecerte mayor seguridad y resguardo, en tu hogar, en tu espacio de trabajo o donde lo necesites. Hacemos instalación y configuración de toda la red. Consultanos para asesorarte sobre la mejor opción.";
 const mantenimientoActualizacion = "La tecnología avanza a pasos agigantados y esta presente en casi todas las áreas de trabajo y entretenimiento. Te asesoramos en tu próximo paso informático o en el cuidado de tu actual equipo. Ahorrá contratiempos y dolores de cabeza; realizamos chequeos y limpieza en tu computadora para que todo funcione correctamente.";
@@ -54,8 +51,6 @@ function showText(currentBtn) {
 }
 
 function defText(currentHover) {
-    textContainer.style.transition = 'opacity 0.3s';
-    textContainer.style.opacity = '0';
     switch (currentHover.id){
     case 'button1':
         click2.style.opacity = '1';
