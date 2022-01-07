bf69f0c2f4033ad5b6d52fb4fc65338976d5195e
TACITVM <tacitvm@tuta.io>
2022-01-07 13:38:39 -0300

    logo placement and some other minor fixes

--- realtime-changes.js
+++ realtime-changes.js
@@ -5,12 +5,12 @@ const click4 = document.getElementById("button4");
 const click5 = document.getElementById("button5");
 const click6 = document.getElementById("button6");
 let textContainer = document.getElementById("interchangableText");
-const soporteTecnico = "Nos encargamos de identificar el problema tanto de software como hardware para brindarte la mejor solución a tu alcance. Reparamos computadoras de escritorio, notebooks y netbooks. Sí tenés un problema de software con algún programa, incluso podemos solucionarlo remotamente. (aca va contacto mas rapido, sea telefono, whatsapp, etc) con un botón CTA";
+const soporteTecnico = "Nos encargamos de identificar el problema tanto de software como hardware para brindarte la mejor solución a tu alcance. Reparamos computadoras de escritorio, notebooks y netbooks. Sí tenés un problema de software con algún programa, incluso podemos solucionarlo remotamente.";
 const seguridadVigilancia = "Trabajamos con equipos de las mejores marcas para darte lo último en tecnología y ofrecerte mayor seguridad y resguardo, en tu hogar, en tu espacio de trabajo o donde lo necesites. Hacemos instalación y configuración de toda la red. Consultanos para asesorarte sobre la mejor opción.";
 const mantenimientoActualizacion = "La tecnología avanza a pasos agigantados y esta presente en casi todas las áreas de trabajo y entretenimiento. Te asesoramos en tu próximo paso informático o en el cuidado de tu actual equipo. Ahorrá contratiempos y dolores de cabeza; realizamos chequeos y limpieza en tu computadora para que todo funcione correctamente.";
 const backups = "¿Te pasó que perdiste algún archivo de suma importancia? Nunca más. Realizamos copias de seguridad para que tengas disponible siempre todo el material que guardes en tu computadora. Con nuestro sistema de enlace podemos recuperar todos los datos que necesites, cuando los necesites.";
-const accesorios = "Tenemos todos los accesorios para tu computadora, teclados, cámaras web, discos externos, pendrives, cables HDMI, adaptadores y muchísimo más. ¡Contactate!";
-const lastItem = "¿Querés darle más visibilidad y profesionalidad a tu negocio? Diseñamos páginas web a medida, ETC";
+const accesorios = "Tenemos todos los accesorios para tu computadora, teclados, cámaras web, discos externos, pendrives, cables HDMI, adaptadores y muchísimo más.";
+const lastItem = "¿Querés darle más visibilidad y profesionalidad a tu negocio? Diseñamos páginas web a medida.";
 
 
 function showText(currentBtn) {
